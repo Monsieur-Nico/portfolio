f7605c678ec17f6449f0acea6560165b66a13503
Monsieur-nico <nicolaskhoury98@gmail.com>
2023-04-15 03:19:35 -0400

    export all the assets

---
+++ src/assets/index.js
@@ -0,0 +1,61 @@
+// Main icons
+import close from "./close.svg";
+import github from "./github.png";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+
+// Technologies
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
+
+// Companies
+import bestBuy from "./company/bestBuy.png";
+import millennium from "./company/millennium1_Solutions.png";
+import rok from "./company/rok_commander.png";
+
+// Projects
+import devsden from "./projects/devsden.svg";
+import interview from "./projects/Interview.svg";
+import textConvert from "./projects/textConvert.svg";
+import uberLabs from "./projects/UberLabs.svg";
+
+
+export {
+  logo,
+  backend,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  bestBuy,
+  millennium,
+  rok,
+  devsden,
+  interview,
+  textConvert,
+  uberLabs
+};
+
