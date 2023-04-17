78dc8bf0c05ea7212cea92402b99ba2b6cf48502
Monsieur-nico <nicolaskhoury98@gmail.com>
2023-04-17 00:32:05 -0400

    Implement Canvas Loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -35,7 +35,7 @@ const ComputerCanvas = () => {
       camera={{ position: [20, 30, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
