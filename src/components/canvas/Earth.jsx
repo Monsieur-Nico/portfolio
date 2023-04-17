2ab4c0a34a35a2d15a1f7ee6cc048ae562770b61
Monsieur-nico <nicolaskhoury98@gmail.com>
2023-04-17 19:46:13 -0400

    Fix PreLoad on Earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 
@@ -17,17 +17,24 @@ const EarthModel = () => {
     <Canvas
       shadows
       frameloop="demand"
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
           enableZoom={false}
-          minPolarAngle={Math.PI / 2}
           maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
         <Earth />
+        <Preload all />
       </Suspense>
     </Canvas>
   );
